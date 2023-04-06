9efed8cb500efff81a1e45ecf22cf038f0b7ac0d
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-04-06 21:32:26 +0300

    Refactor code

--- script.js
+++ script.js
@@ -8,25 +8,34 @@ function getComputerChoice() {
 
 let computerSelection = getComputerChoice();
 
-console.log(computerSelection);
+//console.log(computerSelection, "-computerSelection");
 
 //Get players's choice
-let playerSelection = prompt("Rock, paper, or scissors?");
+let playerSelection = "Rock"; //prompt("Rock, paper, or scissors?");
+
+//console.log(playerSelection, "-playerSelection");
 
 //Create a function that plays a single round
 //If player made the same choice as computer, then print "Draw"
 //If player has rock and computer has scissors OR player has paper and computer has rock OR player has scissors and computer has paper, then print "You win"
 //If it's the other way around, print "You lose"
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return `Draw! Both chose ${playerSelection}.`;
+  let selection1 = playerSelection.toLowerCase();
+  let selection2 = computerSelection.toLowerCase();
+
+  console.log(selection1, selection2);
+
+  if (selection1 === selection2) {
+    return `Draw! Both chose ${selection1}.`;
   }
-  else if (playerSelection === "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
-    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  else if (selection1 === "rock" && selection2 == "scissors" || selection1 == "paper" && selection2 == "rock" || selection1 == "scissors" && selection2 == "paper") {
+    return `You win! ${selection1} beats ${selection2}.`;
   }
-  else if (playerSelection === "Scissors" && computerSelection == "Rock" || playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors") {
-    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+  else if (selection1 === "scissors" && selection2 == "rock" || selection1 == "rock" && selection2 == "paper" || selection1 == "paper" && selection2 == "scissors") {
+    return `You lose! ${selection2} beats ${selection1}.`;
+  } else {
+    console.log("Error");
   }
 }
 
-alert(playRound(Rock, Paper));
+console.log(playRound(playerSelection, computerSelection));
