496498609924b9256c5dfe02f066c0e59c3c0f05
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-04-08 19:35:58 +0300

    Create function game()

--- script.js
+++ script.js
@@ -26,16 +26,23 @@ function playRound(playerSelection, computerSelection) {
   console.log(selection1, selection2);
 
   if (selection1 === selection2) {
-    return `Draw! Both chose ${selection1}.`;
+    console.log(`Draw! Both chose ${selection1}.`);
   }
-  else if (selection1 === "rock" && selection2 == "scissors" || selection1 == "paper" && selection2 == "rock" || selection1 == "scissors" && selection2 == "paper") {
-    return `You win! ${selection1} beats ${selection2}.`;
+  else if (selection1 === "rock" && selection2 === "scissors" || selection1 === "paper" && selection2 === "rock" || selection1 === "scissors" && selection2 === "paper") {
+    console.log(`You win! ${selection1} beats ${selection2}.`);
   }
-  else if (selection1 === "scissors" && selection2 == "rock" || selection1 == "rock" && selection2 == "paper" || selection1 == "paper" && selection2 == "scissors") {
-    return `You lose! ${selection2} beats ${selection1}.`;
+  else if (selection1 === "scissors" && selection2 === "rock" || selection1 === "rock" && selection2 === "paper" || selection1 === "paper" && selection2 === "scissors") {
+    console.log(`You lose! ${selection2} beats ${selection1}.`);
   } else {
     console.log("Error");
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+
+//Write a new function to play 5 rounds and display the results of each round and the winner at the end.
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound(playerSelection, computerSelection);
+  }
+}
