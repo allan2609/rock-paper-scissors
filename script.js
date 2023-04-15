d34a344535cbf5260fa16db928202a5267e84129
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-04-15 22:21:42 +0300

    Create getPlayerChoice function

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-//Create array of possible choices
 const myArray = ["Rock", "Paper", "Scissors"];
 
-//Create function to get computer's choice
 function getComputerChoice() {
   return myArray[Math.floor(Math.random() * myArray.length)];
-}
+};
 
-let computerSelection = getComputerChoice();
+function getPlayerChoice() {
+  return "Rock";
+};
 
-//console.log(computerSelection, "-computerSelection");
-
-//Get players's choice
-let playerSelection = "Rock"; //prompt("Rock, paper, or scissors?");
-
-//console.log(playerSelection, "-playerSelection");
-
-//Create a function that plays a single round
-//If player made the same choice as computer, then print "Draw"
-//If player has rock and computer has scissors OR player has paper and computer has rock OR player has scissors and computer has paper, then print "You win"
-//If it's the other way around, print "You lose"
 function playRound(playerSelection, computerSelection) {
   let selection1 = playerSelection.toLowerCase();
   let selection2 = computerSelection.toLowerCase();
@@ -36,13 +25,14 @@ function playRound(playerSelection, computerSelection) {
   } else {
     console.log("Error");
   }
-}
+};
 
-// console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
 
-//Write a new function to play 5 rounds and display the results of each round and the winner at the end.
+//Display the results of each round
+//Display the winner at the end.
 function game() {
   for (let i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, getComputerChoice());
   }
-}
+};
